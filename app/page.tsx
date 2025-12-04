8c8bfb9f2bb11939af395affea29341205a6c39e
SarveshBhoite <rajb81008@gmail.com>
2025-12-04 22:02:57 +0530

    ui started with hero section

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import HeroSection from "@/components/HeroSection";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-[#050B21]">
+      <HeroSection />
+    </main>
+  );
+}
