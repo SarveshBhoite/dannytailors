ee236a79ced76c056180ce96798990690ceff119
SarveshBhoite <rajb81008@gmail.com>
2025-12-08 11:10:50 +0530

    new pages started

---
+++ app/products/[category]/[productid]/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+import FooterSection from "@/components/Footer";
+
+export default function Shop() {
+  return (
+    <main className="min-h-screen bg-[#050B21]">
+      <Navbar />
+      <FooterSection />
+    </main>
+  );
+}
